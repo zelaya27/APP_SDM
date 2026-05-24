6b87c68dc6a0233da09c4fce55e95311dc62fb45
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-05-24 14:33:10 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,11 @@
 const CONFIG = {
     URL_APPS_SCRIPT: 'https://script.google.com/macros/s/AKfycbyJS_Ia531W0hdIB6ZZoQam_b0M4Kgr85iibOdX4dnHZuIzH3Xe0GAqXHpccNciTKEG/exec'
 };
+function normalizarDatos(datos) {
+    const limpios = {};
+    for (let key in datos) {
+        const valor = datos[key];
+        limpios[key] = (valor === undefined || valor === null) ? "" : valor;
+    }
+    return limpios;
+}
