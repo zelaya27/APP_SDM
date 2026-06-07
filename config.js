c97a0aea2197ae00caae62ab3fbb97161afe1a6c
zelaya27 <68571960+zelaya27@users.noreply.github.com>
2026-06-06 23:43:07 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    URL_APPS_SCRIPT: 'https://script.google.com/macros/s/AKfycbyBwi0QDI5QzxE_9BoD2Hr4CVDh-QomX_p9l5PSquqmf8o2O-5toKWvlENolOuoXP-3/exec'
+    URL_APPS_SCRIPT: 'https://script.google.com/macros/s/AKfycbx7i1NR_uwtLcMj3LUrqal6Nsy4CdsZARkfraEVhd5ziINnYDG62LkLc8XPV9IurndW/exec'
 };
 function normalizarDatos(datos) {
     const limpios = {};
